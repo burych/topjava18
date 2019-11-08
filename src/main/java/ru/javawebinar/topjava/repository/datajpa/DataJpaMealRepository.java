72f7cd77adb134525af4f5bc339c4cf7e8acd4ea
Burych <denis.burtsev@mail.ru>
2019-11-08 20:55:00 +0300

    6_0_fix.patch

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -2,10 +2,14 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.DateTimeUtil;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
@@ -13,29 +17,45 @@ public class DataJpaMealRepository implements MealRepository {
 
     @Autowired
     private CrudMealRepository crudRepository;
+    @Autowired
+    private CrudUserRepository userRepository;
 
     @Override
     public Meal save(Meal meal, int userId) {
+        if(meal.isNew() || get(meal.getId(), userId)!= null){
+            meal.setUser(userRepository.getOne(userId));
+            return crudRepository.save(meal);
+        }
         return null;
     }
 
     @Override
     public boolean delete(int id, int userId) {
+        Meal meal = get(id, userId);
+        if (meal != null){
+            crudRepository.delete(meal);
+            return true;
+        }
         return false;
     }
 
     @Override
     public Meal get(int id, int userId) {
+
+        Meal meal = crudRepository.findById(id).orElse(null);
+        if (meal != null && meal.getUser().getId() == userId){
+            return meal;
+        }
         return null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.getAllOfUser(userId);
     }
 
     @Override
     public List<Meal> getBetweenInclusive(LocalDate startDate, LocalDate endDate, int userId) {
-        return null;
+        return crudRepository.getBetweenInclusive(DateTimeUtil.getStartInclusive(startDate), DateTimeUtil.getEndExclusive(endDate), userId);
     }
 }
