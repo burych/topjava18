a3af3b9dcd968e7edf4f0a3b6859a0d0eb1904f1
Burych <denis.burtsev@mail.ru>
2019-11-08 21:21:16 +0300

    6_01_HW5_data_jpa.patch

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,19 +1,7 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.javawebinar.topjava.model.Meal;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
-
-    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.id DESC")
-    List<Meal> getAllOfUser(@Param("userId") int userId);
-
-    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime >= :startDate AND m.dateTime < :endDate ORDER BY m.dateTime DESC")
-    List<Meal> getBetweenInclusive(@Param("startDate") LocalDateTime startDateTime, @Param("endDate") LocalDateTime endDateTime, @Param("userId") int id);
-
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -2,14 +2,10 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
-import ru.javawebinar.topjava.util.DateTimeUtil;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 
 @Repository
@@ -17,45 +13,29 @@ public class DataJpaMealRepository implements MealRepository {
 
     @Autowired
     private CrudMealRepository crudRepository;
-    @Autowired
-    private CrudUserRepository userRepository;
 
     @Override
     public Meal save(Meal meal, int userId) {
-        if(meal.isNew() || get(meal.getId(), userId)!= null){
-            meal.setUser(userRepository.getOne(userId));
-            return crudRepository.save(meal);
-        }
         return null;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        Meal meal = get(id, userId);
-        if (meal != null){
-            crudRepository.delete(meal);
-            return true;
-        }
         return false;
     }
 
     @Override
     public Meal get(int id, int userId) {
-
-        Meal meal = crudRepository.findById(id).orElse(null);
-        if (meal != null && meal.getUser().getId() == userId){
-            return meal;
-        }
         return null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return crudRepository.getAllOfUser(userId);
+        return null;
     }
 
     @Override
     public List<Meal> getBetweenInclusive(LocalDate startDate, LocalDate endDate, int userId) {
-        return crudRepository.getBetweenInclusive(DateTimeUtil.getStartInclusive(startDate), DateTimeUtil.getEndExclusive(endDate), userId);
+        return null;
     }
 }
